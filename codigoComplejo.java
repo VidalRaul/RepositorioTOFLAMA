e68c887e0a4d54b33c7c8906f1802da34e8091b5
VidalRaul <raulvidal@paucasesnovescifp.cat>
2020-05-13 12:09:45 +0200

    Primera version Compleja

---
+++ codigoComplejo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        
+        System.out.println("Codigo Complejo, aun mas complejo todavia");
+    }
+}
